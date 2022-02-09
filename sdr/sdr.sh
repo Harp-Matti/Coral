d2bf753c580802209b5cc1c78c6839b586e86c19
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-02-09 16:00:10 +0200

    Update sdr.sh

--- sdr/sdr.sh
+++ sdr/sdr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 swig -c++ -python -py3 sdr.i
-g++ -c sdr_wrap.cxx -I/usr/include/python3.7m -lSoapySDR
+g++ -c -fPIC sdr_wrap.cxx -I/usr/include/python3.7m -lSoapySDR
 g++ -shared sdr_wrap.o -o _sdr.so -lSoapySDR
