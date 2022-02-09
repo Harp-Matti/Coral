ac8a7480a9555ff1320b06506d68e3624a0beb6b
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-02-09 12:14:37 +0200

    Update sdr.h

--- sdr/sdr.h
+++ sdr/sdr.h
@@ -26,6 +26,7 @@ public:
 		}
 		
 		ranges = sdr->getFrequencyRange( SOAPY_SDR_RX, 0);
+		setBufferSize(1024);
 		
 		rx_stream = sdr->setupStream( SOAPY_SDR_RX, SOAPY_SDR_CF32);
 		if(rx_stream == NULL)
@@ -46,7 +47,7 @@ public:
 		void *buffs[] = {buff};
 		int flags;
 		long long time_ns;
-		int ret = sdr->readStream(rx_stream, buffs, 1024, flags, time_ns, 1e5);
+		int ret = sdr->readStream(rx_stream, buffs, buff.size(), flags, time_ns, 1e5);
 		//printf("ret = %d, flags = %d, time_ns = %lld\n", ret, flags, time_ns);
 		return ret;
 	}
@@ -67,6 +68,15 @@ public:
 		return sdr->getFrequency(SOAPY_SDR_RX, 0);
 	}
 	
+	int getBufferSize(){
+		return buff.size();
+	}
+	
+	void setBufferSize(int size){
+		std::complex<float> new_buffer(size); 
+		buff = new_buffer;	
+	}
+	
 	void setFrequency(double freq){
 		if (freq >= ranges[0].minimum() && freq <= ranges[0].maximum()){
 			sdr->setFrequency(SOAPY_SDR_RX, 0, freq);
@@ -78,5 +88,5 @@ public:
 	SoapySDR::Device *sdr;
 	SoapySDR::RangeList ranges;
 	SoapySDR::Stream *rx_stream;
-	std::complex<float> buff[1024];
+	std::complex<float> buff;
 };
