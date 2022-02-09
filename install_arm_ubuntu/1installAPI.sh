ef16599fcf67581aa0e1b7e172ea8da288704b03
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-02-09 09:11:11 +0200

    Update 1installAPI.sh

--- install_arm_ubuntu/1installAPI.sh
+++ install_arm_ubuntu/1installAPI.sh
@@ -7,6 +7,8 @@
 
 # clear the terminal
 clear
+sudo apt-get update
+sudo apt-get dist-upgrade
 # say what we are doing
 echo "SDRplay script to download and run the RSP API Installer - v0.3"
 # copy restart script
