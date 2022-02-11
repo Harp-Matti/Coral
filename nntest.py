2af4ac072ed55f6af39150fc2f1745b117a2c1d8
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-02-11 13:28:33 +0200

    Update nntest.py

--- nntest.py
+++ nntest.py
@@ -46,8 +46,8 @@ start = timer()
 for i in range(N_classifications):
     if device.receive() is not 1024:
         print('Receive failed')
+    device.setFrequency(1.0e9+(i+1)*10.0e6)    
     runClassifier(interpreter,labels,normalize(np.asarray(device.read()).reshape((2,1024))))
-    device.setFrequency(1.0e9+(i+1)*10.0e6)
 
 end = timer()
 print(f'Average inference time over {N_classifications} samples: {(end-start)/N_classifications} seconds')
