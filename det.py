f31196f30a65ab27159e4f40533b8cf3a6d60996
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-02-15 10:34:09 +0200

    Update det.py

--- det.py
+++ det.py
@@ -19,13 +19,17 @@ eps = 1.0e-10
 def kurt(x):
     s = x[0,:]+1j*x[1,:]
     s = s-np.mean(s)
-    return np.mean(np.power(s,4))/np.mean(np.power(s,2))
+    cs = np.conj(s)    
+    v = np.mean(s*cs)    
+    s20 = np.absolute(np.mean(np.power(s,2))/(v+eps))
+    s22 = np.absolute(np.mean(np.power(s,2)*np.power(cs,2))/(np.power(v,2)+eps))    
+    return (s22-2-np.power(s20,2))/(1+np.power(s20,2)/2)
 
 N_classifications = 11
 #receive some samples
 for i in range(N_classifications):
     if device.receive() < N_samples:
         print('Receive failed')
-    freq = 1.0e9+(i+1)*10.0e6    
+    freq = 1.0e9+i*10.0e6    
     device.setFrequency(freq)    
     print('Kurtosis ' + str(kurt(np.asarray(device.read()).reshape((2,N_samples)))) + ' at frequency ' + str(round(freq/1e6)) + ' MHz')
