e3557e127bc3c2dda3d942ab04c839bd9fb666fd
Matti <matti.mikkola@harptechnologies.com>
2022-05-03 11:27:51 +0300

    asd

--- comms/comms.py
+++ comms/comms.py
@@ -2,7 +2,37 @@ import socket
 import sys
 import time
 from struct import pack, unpack, unpack_from
-from message import Message, Failure, Success, Run, Result, Get, Set
+
+class Message:
+    pass
+    
+class Failure(Message):    
+    pass
+    
+class Success(Message):
+    pass
+    
+class Run(Message):
+    pass
+    
+class Result(Message):
+    def __init__(self,class_result,spectrum_result):
+        self.class_result = class_result
+        self.spectrum_result = spectrum_result
+        
+class Get(Message):
+    def __init__(self,parameter):
+        self.parameter = parameter
+        
+class Set(Message):
+    def __init__(self,parameter,value):
+        self.parameter = parameter
+        self.value = value
+        
+class Return(Message):
+    def __init__(self,parameter,value):
+        self.parameter = parameter
+        self.value = value
 
 class Comms:
     def __init__(self,host,port):   
