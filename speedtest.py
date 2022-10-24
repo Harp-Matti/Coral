17d28e3e32ee6b5fef1304a90d8d0ae2a2be4859
Harp-Matti <62238577+Harp-Matti@users.noreply.github.com>
2022-10-24 11:20:53 +0300

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -14,14 +14,14 @@ model_files = [None]*5
 if edge:
     print("Edge TPU detected")
     from coralclassifier import *    
-    model_file[0] = os.path.join(script_dir, 'model_hfradio_resnet_maxnorm_qaware_quant_edgetpu.tflite')
-    model_file[1] = os.path.join(script_dir, 'model_hfradio_deepcnn_maxnorm_qaware_quant_edgetpu.tflite')
-    model_file[2] = os.path.join(script_dir, 'model_hfradio_resnetos_maxnorm_qaware_quant_edgetpu.tflite')
+    model_files[0] = os.path.join(script_dir, 'model_hfradio_resnet_maxnorm_qaware_quant_edgetpu.tflite')
+    model_files[1] = os.path.join(script_dir, 'model_hfradio_deepcnn_maxnorm_qaware_quant_edgetpu.tflite')
+    model_files[2] = os.path.join(script_dir, 'model_hfradio_resnetos_maxnorm_qaware_quant_edgetpu.tflite')
 else:
     from tfclassifier import *
-    model_file[0] = os.path.join(script_dir, 'model_hfradio_resnet_maxnorm_qaware_quant.tflite')
-    model_file[1] = os.path.join(script_dir, 'model_hfradio_deepcnn_maxnorm_qaware_quant.tflite')
-    model_file[2] = os.path.join(script_dir, 'model_hfradio_resnetos_maxnorm_qaware_quant.tflite')
+    model_files[0] = os.path.join(script_dir, 'model_hfradio_resnet_maxnorm_qaware_quant.tflite')
+    model_files[1] = os.path.join(script_dir, 'model_hfradio_deepcnn_maxnorm_qaware_quant.tflite')
+    model_files[2] = os.path.join(script_dir, 'model_hfradio_resnetos_maxnorm_qaware_quant.tflite')
     
 import sys
 from time import sleep
@@ -34,8 +34,8 @@ from timeit import default_timer as timer
 
 eps = 1.0e-10
 
-model_file[3] = os.path.join(script_dir, 'model_hfradio_sgd_pwelch_norm_map_128.joblib')
-model_file[4] = os.path.join(script_dir, 'model_adaboost_11.joblib')
+model_files[3] = os.path.join(script_dir, 'model_hfradio_sgd_pwelch_norm_map_128.joblib')
+model_files[4] = os.path.join(script_dir, 'model_adaboost_11.joblib')
 
 def normalize(x):
     x /= np.sqrt(np.amax(np.sum(np.power(x,2),axis=0,keepdims=False)))+eps
@@ -50,11 +50,11 @@ class Sensor:
         self.rng = default_rng() 
           
         self.classifiers = []
-        self.classifiers.append(NeuralNet(model_file[0]))
-        self.classifiers.append(NeuralNet(model_file[1]))
-        self.classifiers.append(NeuralNet(model_file[2]))
-        self.classifiers.append(SVM(model_file[3]))
-        self.classifiers.append(RandomForest(model_file[4]))
+        self.classifiers.append(NeuralNet(model_files[0]))
+        self.classifiers.append(NeuralNet(model_files[1]))
+        self.classifiers.append(NeuralNet(model_files[2]))
+        self.classifiers.append(SVM(model_files[3]))
+        self.classifiers.append(RandomForest(model_files[4]))
         print('Classifiers loaded')
         
     def run(self,index): 
